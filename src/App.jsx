76902ef76ae98fdb31089b6f42fecb85643752dc
serenade18 <seretaahmed18@gmail.com>
2025-03-05 16:20:03 +0300

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, lazy, Suspense  } from 'react'
+import './assets/style/style.css'
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom'
+
+
+
+
+// Lazy-loaded components
+const Login = lazy(() => import('./pages/auth/login'));
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+        <Routes>
+            {/* Auth Pages */}
+            <Route exact path="/" element={<Login />}/>
+        </Routes>
+    </Router>
   )
 }
 
